ca6f8d967dddb01f8ff9c389d615817e46613ab6
Francis Asante <kofrasa@gmail.com>
2015-12-15 09:19:19 +0200

    added tmux conf and general install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+DIR=`dirname $0`
+ARG=$1
+OPT=`ls $DIR | grep -v 'install.sh' | sort`
+
+# validate directory
+if test -z "$ARG"; then
+    echo "Specify install directory: See options below"
+    echo "$OPT"
+    exit 1
+fi
+
+BASE="$DIR/$ARG"
+if test ! -d $BASE; then
+    echo "Invalid directory '$ARG': See options below"
+    echo "$OPT"
+    exit 1
+fi
+
+echo "installing '$ARG' config..."
+
+if test -f "$BASE/install.sh"; then
+    sh "$BASE/install.sh"
+else
+    for conf in `ls $BASE`; do
+        cp "$BASE/$conf" "$HOME/.$conf"
+    done
+fi
+
+echo "done"
