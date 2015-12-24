768fdd28f4f093056497499d0cd7f4bed1fcd814
Francis Asante <kofrasa@gmail.com>
2015-12-24 16:28:55 +0200

    add ctags and perform batch install

--- install.sh
+++ install.sh
@@ -1,32 +1,15 @@
 #!/usr/bin/env bash
-set -e
 
 DIR=`dirname $0`
-ARG=$1
-shift
-OPT=`ls $DIR | grep -v 'install.sh' | sort`
 
-# validate directory
-if test -z "$ARG"; then
-    echo "Specify install directory: See options below"
-    echo "$OPT"
-    exit 1
-fi
-
-BASE="$DIR/$ARG"
-if test ! -d $BASE; then
-    echo "Invalid directory '$ARG': See options below"
-    echo "$OPT"
-    exit 1
-fi
-
-echo "installing '$ARG' config..."
-
-if test -f "$BASE/install.sh"; then
-    exec "$BASE/install.sh" $@
-else
-    for conf in `ls $BASE`; do
-        cp "$BASE/$conf" "$HOME/.$conf"
-    done
-fi
+for name in $(ls $DIR | grep -v "`basename $0`"); do
+    echo "installing $name"
+    if [ -d $name ]; then
+        cd "$DIR/$name"
+        source "./install.sh" $*
+        cd $DIR
+    elif [ -f $name ]; then
+        cp "$name" "$HOME/.$name"
+    fi
+done
 
